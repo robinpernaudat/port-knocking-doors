86f449e7186cb088a4081caecedeb4442d243dbb
Robin <robin.pernaudat@gmail.com>
2022-02-21 18:46:03 +0100

    débogage des fonctions pour créer la "zone" du firewall

--- src/firewall.rs
+++ src/firewall.rs
@@ -5,7 +5,7 @@
 //! The zone and rules aren't permenent. So il the firewall is restarted, the zone will be create again
 //! and the clients have to réopen there ports.
 
-use log::info;
+use log::{info, debug};
 use std::net::IpAddr;
 use std::process::Command;
 use std::time::Duration;
@@ -33,7 +33,7 @@ pub fn init() {
         .output()
         .expect("failed to execute process");
     let whiche_one: String = String::from_utf8(cmd_exec.stdout).unwrap();
-    let wall = if whiche_one == String::from("firewalld") {
+    let wall = if whiche_one.eq("firewalld\n") {
         FirewallType::FIREWALLD
     }
     // else if whiche_one == "iptables"{
@@ -85,6 +85,7 @@ fn is_firewall_zone_exists() -> bool {
     info!("Checking if the firewall zone exists");
     let result = match unsafe { FIREWALL_TYPE.clone() } {
         FirewallType::FIREWALLD => Command::new("bash")
+            .arg("-c")
             .arg("firewall-cmd --list-all-zones | egrep \"^knock-access\"")
             .output()
             .expect("error wil creating a zone"),
@@ -100,14 +101,17 @@ fn is_firewall_zone_exists() -> bool {
 
 fn prepare_firewall_zone() {
     info!("preparing the firewall zone");
-    let _result = match unsafe { FIREWALL_TYPE.clone() } {
+    let result = match unsafe { FIREWALL_TYPE.clone() } {
         FirewallType::FIREWALLD => Command::new("firewall-cmd")
             .arg("--new-zone=knock-access")
+            .arg("--permanent")
             .output()
             .expect("error while creating a zone"),
         //FirewallType::IPTABLES => Command::new("").output().expect("error wil creating a zone"),,
         _ => panic!("This app can't work if it can't control the firewall."),
     };
+    firewall_cmd_reload();
+    debug!("returned : {}  |  {}", String::from_utf8(result.stdout).unwrap(), String::from_utf8(result.stderr).unwrap());
 }
 
 fn drop_firewall_zone() {
@@ -115,12 +119,27 @@ fn drop_firewall_zone() {
     let _result = match unsafe { FIREWALL_TYPE.clone() } {
         FirewallType::FIREWALLD => Command::new("firewall-cmd")
             .arg("--delete-zone=knock-access")
+            .arg("--permanent")
             .output()
             .expect("error while removing a zone"),
         //FirewallType::IPTABLES => Command::new("").output().expect("error wil creating a zone"),,
         _ => panic!("This app can't work if it can't control the firewall."),
     };
+    firewall_cmd_reload();
 }
+
+fn firewall_cmd_reload(){
+    info!("reloading the firewall conf");
+    let _result = match unsafe { FIREWALL_TYPE.clone() } {
+        FirewallType::FIREWALLD => Command::new("firewall-cmd")
+            .arg("--reload")
+            .output()
+            .expect("error while reloading the firewall"),
+        //FirewallType::IPTABLES => Command::new("").output().expect("error wil creating a zone"),,
+        _ => panic!("This app can't work if it can't control the firewall."),
+    };
+}
+
 
 fn add_ports_list_to_the_firewall_zone() {
     info!("configuring the port of the firewall's zone.");
@@ -143,9 +162,11 @@ fn add_ports_list_to_the_firewall_zone() {
 pub fn checkup(reset: bool) {
     info!("firewall checkup");
     if !is_firewall_zone_exists() {
+        debug!("the firewall zone doesn't exists");
         prepare_firewall_zone();
         add_ports_list_to_the_firewall_zone();
     } else if reset {
+        debug!("the firewall zone exists");
         drop_firewall_zone();
         checkup(false);
     }
