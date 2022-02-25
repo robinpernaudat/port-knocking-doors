c9430707654b0ecf52e778a57f7c013a98f5a401
Robin <robin.pernaudat@gmail.com>
2022-02-25 15:22:31 +0100

    suppression du script qui servait pour la validation

--- default-knock-sequence.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "knock" | nc -u -w 1 127.0.0.1 16001
-echo "knock" | nc -u -w 1 127.0.0.1 16002
-echo "knock" | nc -u -w 1 127.0.0.1 16003
-echo "knock" | nc -u -w 1 127.0.0.1 16004
-echo "knock" | nc -u -w 1 127.0.0.1 16005
-
-echo "firewall-cmd --info-zone=knock-access"
